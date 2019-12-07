6e3c4170747bc6adfec4b79eb99b60cd3e50f956
Vlad-Kostyuk <vlad.kostyuk24@gmail.com>
2019-12-07 16:03:35 +0200

    lab-10 add url module

--- node/server.js
+++ node/server.js
@@ -1,8 +1,23 @@
 var http= require("http");
+var url = require('url');
 
  http.createServer(function(request, response) {
-   response.writeHead(200, {'Content-Type': 'text/html'});
-   response.end('Testing NodeJSserver\n');
+  var q = url.parse(request.url, true);
+   var filename =  q.pathname;
+   
+   if (filename != '/fans' && filename != '/news') {
+      response.writeHead(404, {'Content-Type': 'text/html'});
+      return response.end("404 Not Found");
+    } else {  
+       response.writeHead(200, {'Content-Type': 'text/html'});
+       response.end('Testing NodeJSserver\n' + ' ' + filename);
+	}
+   
  }).listen(8000);
  
 console.log('Server running at http://127.0.0.1:8000/');
+
+
+
+
+
